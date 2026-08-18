dfb9300cb9c2bf04e025355641683c14b3008bdd
2025ac05837 <2025ac05837@wilp.bits-pilani.ac.in>
2026-08-18 09:26:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ with tab2:
     st.pyplot(fig2)
 
 st.markdown("---")
-st.caption("BITS Pilani WILP — M.Tech — Machine Learning Assignment 2")
+st.caption("BITS Pilani WILP — M.Tech — Machine Learning Assignment 2 By:2025ac05837")
